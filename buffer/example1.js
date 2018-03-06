5e791ee02f0ecdbfdb54864b08e69e1e4ea40c7f
vikash <vikashraj144@gmail.com>
2018-03-06 15:54:44 +0530

    buffer module added

---
+++ buffer/example1.js
@@ -0,0 +1,7 @@
+/* 
+Node.js provides Buffer class to store raw data similar to an array of integers but corresponds to a 
+raw memory allocation outside the V8 heap. Buffer class is used because pure JavaScript is not nice to 
+binary data. So, when dealing with TCP streams or the file system, it's necessary to handle octet streams
+
+*/
+
