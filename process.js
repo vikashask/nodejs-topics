b21c6ea38c548a945c5498960285cedba35a7c1d
vikashraj144 <vikashraj144@gmail.com>
2018-04-12 10:38:49 +0530

    changes

--- process.js
+++ process.js
@@ -1,3 +1,6 @@
+/* 
+The process object is a global that provides information about, and control over, the current Node.js process.
+ */
 var fs = require('fs');
 process.on('uncaughtException', (err) => {
     fs.writeSync(1, `Caught exception: ${err}\n`);
