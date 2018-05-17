875089791761f18df13ea034019e53f05cc1963f
vikash <vikashraj144@gmail.com>
2018-05-17 07:38:35 +0530

    file name

--- zlib/zlib-example2.js
+++
@@ -1,7 +0,0 @@
-const zlib = require('zlib');    
-const unzip = zlib.createUnzip();  
-const fs = require('fs');  
-const inp = fs.createReadStream('input.txt.gz');  
-const out = fs.createWriteStream('input2.txt');  
-  
-inp.pipe(unzip).pipe(out);  
