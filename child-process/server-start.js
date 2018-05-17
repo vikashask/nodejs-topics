07499cc0bea52e869e2d013e4a199d6c59649617
vikash <vikashraj144@gmail.com>
2018-05-17 07:37:51 +0530

    changed

--- child-process/server-start.js
+++
@@ -1,9 +0,0 @@
-// start server from here
-var exec = require('child_process').exec;
-exec('node ./server.js', function(error, stdout, stderr) {
-    console.log('stdout: ', stdout);
-    console.log('stderr: ', stderr);
-    if (error !== null) {
-        console.log('exec error: ', error);
-    }
-});
