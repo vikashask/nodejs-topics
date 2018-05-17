2e6a8a6dc486f041201a34885262fe8d8c25a31b
vikash <vikashraj144@gmail.com>
2018-05-17 07:37:26 +0530

    path ch

--- dns/dns_example3.js
+++
@@ -1,8 +0,0 @@
-/* 
- print the localhost name using lookupService() function
-  */
-const dns = require('dns');  
-dns.lookupService('127.0.0.1', 22, (err, hostname, service) => {  
-  console.log(hostname, service);  
-    // Prints: localhost  
-});  
