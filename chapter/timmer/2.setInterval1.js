936a490aca471db6c8f4e3cbde19c661f95f43f1
vikash verma <vikashraj144@gmail.com>
2018-07-12 17:57:54 +0530

    setinterve

---
+++ chapter/timmer/2.setInterval1.js
@@ -0,0 +1,16 @@
+/* 
+Set timer functions:
+
+setImmediate(): It is used to execute setImmediate.
+setInterval(): It is used to define a time interval.
+setTimeout(): ()- It is used to execute a one-time callback after delay milliseconds.
+
+Clear timer functions:
+
+
+*/
+
+setInterval(function () {
+    console.log("setInterval: Hey! 1 millisecond completed!..");
+}, 1000);
+
