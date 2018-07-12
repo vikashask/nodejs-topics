d6f4fe909c04e046fbe09b5f4faa440183f7de77
vikash verma <vikashraj144@gmail.com>
2018-07-12 17:57:39 +0530

    timmer adder

---
+++ chapter/timmer/1.timmer3.js
@@ -0,0 +1,16 @@
+/* 
+Clear timer functions:
+
+clearImmediate(immediateObject): It is used to stop an immediateObject, as created by setImmediate
+clearInterval(intervalObject): It is used to stop an intervalObject, as created by setInterval
+clearTimeout(timeoutObject): It prevents a timeoutObject, as created by setTimeout
+
+*/
+
+function welcome () {  
+    console.log("Welcome to JavaTpoint!");  
+  }  
+  var id1 = setTimeout(welcome,1000);  
+  var id2 = setInterval(welcome,2000);  
+  // clearTimeout(id1);  
+  clearInterval(id2);  
