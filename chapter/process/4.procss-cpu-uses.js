e89c23c7419d79d6f8581f40144c49977ddb2080
vikash verma <vikashraj144@gmail.com>
2018-07-12 16:08:14 +0530

    cpu uses

---
+++ chapter/process/4.procss-cpu-uses.js
@@ -0,0 +1,9 @@
+
+const startUsage = process.cpuUsage();
+// { user: 38579, system: 6986 }
+
+// spin the CPU for 500 milliseconds
+const now = Date.now();
+while (Date.now() - now < 500);
+
+console.log(process.cpuUsage(startUsage));
