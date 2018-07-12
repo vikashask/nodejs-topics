502c255aa797fe02392977380ffd5f62e113ceaf
vikash verma <vikashraj144@gmail.com>
2018-07-12 16:06:51 +0530

    process added

---
+++ chapter/process/1.process.js
@@ -0,0 +1,14 @@
+/* 
+The process object is a global that provides information about, and control over, the current Node.js process.
+ */
+var fs = require('fs');
+process.on('uncaughtException', (err) => {
+    fs.writeSync(1, `Caught exception: ${err}\n`);
+  });
+  
+  setTimeout(() => {
+    console.log('This will still run.');
+  }, 1000);
+  
+  // Intentionally cause an exception, but don't catch it.
+  nonexistentFunc();
