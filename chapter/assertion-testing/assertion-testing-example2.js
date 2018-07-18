017d08edf0b1a87620c494498300cf44d077d517
vikash verma <vikashraj144@gmail.com>
2018-07-18 16:34:34 +0530

    delete assert

--- chapter/assertion-testing/assertion-testing-example2.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');  
-function add (a, b) {  
-  return a + b;  
-}  
-var expected = add(1,2);  
-assert( expected === 4, 'one plus two is three');  
-
-/* Now you will see the AssertionError. */
