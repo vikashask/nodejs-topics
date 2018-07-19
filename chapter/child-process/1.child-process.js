5fb89655682f33da96d87a240f6b159f7b334162
vikash verma <vikashraj144@gmail.com>
2018-07-19 10:58:24 +0530

    chile process

--- chapter/child-process/1.child-process.js
+++ chapter/child-process/1.child-process.js
@@ -3,9 +3,13 @@
 
 */
 var exec = require('child_process').exec;
-exec('node -v', function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
+exec('node -v', function (error, stdout, stderr) {
+    if (stdout) {
+        console.log('stdout: ' + stdout);
+    }
+    if (stderr) {
+        console.log('stderr: ' + stderr);
+    }
     if (error !== null) {
         console.log('exec error: ' + error);
     }
