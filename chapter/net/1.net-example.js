9ba2f1aeb834855531b59715bf0e518cb48a6ea9
vikash verma <vikashraj144@gmail.com>
2018-07-19 15:31:30 +0530

    net in node

---
+++ chapter/net/1.net-example.js
@@ -0,0 +1,16 @@
+/* 
+The net module provides an asynchronous network API for creating stream-based TCP or IPC servers 
+(net.createServer()) and clients (net.createConnection()).
+ */
+const net = require('net');  
+var server = net.createServer((socket) => {  
+  socket.end('goodbye\n');  
+}).on('error', (err) => {  
+  // handle errors here  
+  throw err;  
+});  
+// grab a random port.  
+server.listen(() => {  
+  address = server.address();  
+  console.log('opened server on %j', address);  
+});  
