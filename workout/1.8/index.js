aca6818eb9a7ff5102e61a790ffa61fbe9228d84
vikash verma <vikashraj144@gmail.com>
2018-07-20 17:41:30 +0530

    index for local

---
+++ workout/1.8/index.js
@@ -0,0 +1,11 @@
+// let exports = module.exports;
+
+// var g = 42; // local to this file.
+
+// require = function() {
+//   return { mocked: true };
+// };
+//
+// const fs = require('fs');
+// console.log(fs);
+
