c8e8200283ad5a14636cdbd29cfcfd8db18a7cd2
vikash verma <vikashraj144@gmail.com>
2018-07-20 17:40:20 +0530

    index

---
+++ workout/1.6/index.js
@@ -0,0 +1,19 @@
+console.log(module);
+
+// node will start looking vikask in module.paths first if not found it will through 
+// const vikAsk = require('vikask');
+
+
+//Cannot find module 'vikask'
+// Just resolve, don't load 
+// require.resolve('vikask');
+
+// Resolve a vikask.js on the same level as index.js
+// const vikAsk = require('./vikask');
+
+// Resolve a vikask.js on the parent level
+// const vikAsk = require('../vikask');
+// const vikAsk = require('vikask');
+
+// Resolve a vikask.js on the root level
+
