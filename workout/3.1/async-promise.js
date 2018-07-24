ff00775b2ef74a185f2263ead2e9ab40a0981038
vikash verma <vikashraj144@gmail.com>
2018-07-24 16:49:47 +0530

    promises

---
+++ workout/3.1/async-promise.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const readFileAsArray = function(file, cb = () => {}) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, function(err, data) {
+      if (err) {
+        reject(err);
+        return cb(err);
+      }
+
+      const lines = data.toString().trim().split('\n');
+      resolve(lines);
+      cb(null, lines);
+    });
+  });
+};
+
+//  call promises
+readFileAsArray('./numbers')
+  .then(lines => {
+    const numbers = lines.map(Number);
+    const oddNumbers = numbers.filter(number => number % 2 === 1);
+    console.log('odd numbers count:', oddNumbers.length);
+  })
+  .catch(console.error);
+
