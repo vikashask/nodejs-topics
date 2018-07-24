b0a5773561912bac1cfd3696482be676839d6895
vikash verma <vikashraj144@gmail.com>
2018-07-24 16:49:10 +0530

    callback

---
+++ workout/3.1/async-callback.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const readFileAsArray = function(file, cb) {
+  fs.readFile(file, function(err, data) {
+    if (err) {
+      return cb(err);
+    }
+
+    const lines = data.toString().trim().split('\n');
+    // return lines
+    cb(null, lines);
+  });
+};
+
+// example call
+readFileAsArray('./numbers', (err, lines) => {
+  
+  if (err) throw err;
+
+  const numbers = lines.map(Number);
+  console.log(numbers);
+  
+  const oddNumbers = numbers.filter(number => number % 2 === 1);
+  console.log('odd numbers count:', oddNumbers.length);
+});
