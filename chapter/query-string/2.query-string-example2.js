97be863c6f336d46e7d1650b932b4d74c0c99024
vikash <vikashraj144@gmail.com>
2018-07-30 23:23:20 +0530

    query string

--- chapter/query-string/2.query-string-example2.js
+++ chapter/query-string/2.query-string-example2.js
@@ -4,7 +4,13 @@ querystring = require('querystring');
 const qs1=querystring.stringify({name:'vikash',company:'vikask'});  
 console.log(qs1);       
 
-const exp2= querystring.stringify({ foo: 'bar', baz: ['qux', 'quux'], corge: '' });
+const param = { 
+    foo: 'bar',
+    baz: ['qux', 'quux'], 
+    corge: '' 
+};
+
+const exp2= querystring.stringify(param);
 // returns 'foo=bar&baz=qux&baz=quux&corge='
 console.log(exp2);
 
