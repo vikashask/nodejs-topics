5279dbe1e409143f492d8b3b122dd4444ea0ea75
vikash <vikashraj144@gmail.com>
2018-07-30 23:35:37 +0530

    write stream

--- chapter/streams/2.createWriteStream.js
+++ chapter/streams/2.createWriteStream.js
@@ -1,9 +1,15 @@
 var fs = require("fs");  
-var data = 'Hello world';  
+var data = 'Hello world'; 
+
+const details = {
+    name:'vikas',
+    location:'hyd'
+};
+var da = JSON.stringify(details);
 // Create a writable stream  
-var writerStream = fs.createWriteStream('output.txt');  
+var writerStream = fs.createWriteStream('vikask.txt');  
 // Write the data to stream with encoding to be utf8  
-writerStream.write(data,'UTF8');  
+writerStream.write(da,'UTF8');  
 // Mark the end of file  
 writerStream.end();  
 // Handle stream events --> finish, and error  
