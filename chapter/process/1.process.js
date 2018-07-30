234adf28643295f0e96c2d57eabf3b34c2dca9bd
vikash <vikashraj144@gmail.com>
2018-07-30 23:22:28 +0530

    process

--- chapter/process/1.process.js
+++ chapter/process/1.process.js
@@ -2,6 +2,11 @@
 The process object is a global that provides information about, and control over, the current Node.js process.
  */
 var fs = require('fs');
+console.log("ertyuio");
+console.log(process.cwd());
+console.log(process.env);
+
+
 process.on('uncaughtException', (err) => {
     fs.writeSync(1, `Caught exception: ${err}\n`);
   });
